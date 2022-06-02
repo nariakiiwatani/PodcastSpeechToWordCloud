cca54c4155ef0aeec41bba365690c2f3cc0b7051
nariakiiwatani <nariakiiwatani@annolab.com>
2022-06-02 16:25:17 +0900

    Revert "ad tailwindcss"
    This reverts commit 7e65ecc8f405636c3fbf6d15030c47a5471866c6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useCallback } from 'react';
+import './App.css';
 import { Tokenizer, NoTokenizer } from './components/Tokenizer';
 import MyWordCloud from './components/MyWordcloud';
 import Speech2Text from './components/Speech2Text';
